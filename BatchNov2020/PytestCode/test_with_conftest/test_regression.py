ca7119880d5ddbc2dd956809da57f2bbb3fc02bf
deepeshyadav2201@gmail.com <deepeshyadav2201@gmail.com>
2021-01-28 08:58:53 +0530

    New Test Cases Added in Regression Suites

--- BatchNov2020/PytestCode/test_with_conftest/test_regression.py
+++ BatchNov2020/PytestCode/test_with_conftest/test_regression.py
@@ -4,3 +4,8 @@ def test_downpayment(setup, teardown):
     setup.find_element_by_id("cdownpayment").clear()
     setup.find_element_by_id("cdownpayment").send_keys(40)
     sleep(5)
+
+def test_downpayment_2(setup, teardown):
+    setup.find_element_by_id("cdownpayment").clear()
+    setup.find_element_by_id("cdownpayment").send_keys(40)
+    sleep(5)
