c3ebf966b7e50a0c6fc67464fdfd203b652e2bfd
deepeshyadav2201@gmail.com <deepeshyadav2201@gmail.com>
2021-01-29 10:12:33 +0530

    Added new code for framework

--- BatchNov2020/PytestCode/test_with_conftest/test_interest.py
+++ BatchNov2020/PytestCode/test_with_conftest/test_interest.py
@@ -4,3 +4,9 @@ def test_loan_term(setup, teardown):
     setup.find_element_by_id("cloanterm").clear()
     setup.find_element_by_id("cloanterm").send_keys(40)
     sleep(5)
+
+
+def test_loan_term_2(setup, teardown):
+    setup.find_element_by_id("cloanterm").clear()
+    setup.find_element_by_id("cloanterm").send_keys(40)
+    sleep(5)
---
+++ liveproject-auto/src/locators.py
@@ -0,0 +1,3 @@
+
+SEARCH_BOX = "//input[@name='q']"
+SEARCH_BUTTON = "(//input[@name='btnK'])[2]"
